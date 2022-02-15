1938eedb1083640b4b80fc556a68b26bc94ac536
Kurisitofu1205 <72328312+Kurisitofu1205@users.noreply.github.com>
2022-02-15 19:20:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("theme-toggle").addEventListener("change", function() {
-  document.body.classList.toggle("light-theme");
-  document.body.classList.toggle("dark-theme");
-});
-
-
